1b9227f2085c37977c3912fee44c39700c7f89e2
Mattias Duarte <duartemattias4@gmail.com>
2025-06-17 08:45:51 -0400

    chore: update prisma client

---
+++ prisma/migrations/20250615045504_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "OutputType" AS ENUM ('SALE', 'QUOTATION', 'REPLACEMENT', 'CONSIGNMENT');
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "description" TEXT,
+    "clientId" TEXT NOT NULL,
+    "clientName" TEXT NOT NULL,
+    "deliveryMethod" TEXT NOT NULL,
+    "outputType" TEXT NOT NULL,
+    "placeOfIssue" TEXT NOT NULL,
+    "warehouseId" TEXT,
+    "warehouseName" TEXT,
+    "total" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ExpenseItem" (
+    "id" TEXT NOT NULL,
+    "expenseId" TEXT NOT NULL,
+    "productId" TEXT,
+    "productName" TEXT,
+    "productPrice" DOUBLE PRECISION,
+    "productTotal" DOUBLE PRECISION,
+    "productQty" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ExpenseItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Expense_code_key" ON "Expense"("code");
+
+-- CreateIndex
+CREATE INDEX "ExpenseItem_expenseId_idx" ON "ExpenseItem"("expenseId");
+
+-- AddForeignKey
+ALTER TABLE "ExpenseItem" ADD CONSTRAINT "ExpenseItem_expenseId_fkey" FOREIGN KEY ("expenseId") REFERENCES "Expense"("id") ON DELETE CASCADE ON UPDATE CASCADE;
